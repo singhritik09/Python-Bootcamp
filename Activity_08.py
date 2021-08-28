cdfbe6c3b59cace1dce5d1663a289de2c221d671
singhritik09 <70894918+singhritik09@users.noreply.github.com>
2021-08-28 23:33:42 +0530

    Done.

---
+++ Activity_08.py
@@ -0,0 +1,7 @@
+
+1.)Python is an interpreted language.It has it’s own set of libraries written in C,which does the backend work when the code is run.
+Code written in python is interpreted not compiled that means the code is not converted to assembly code when runned,it gets executed line by line.
+
+2.)Compiler converts the man written code to the machine code for execution,whereas an interpreter directly executes the code line by line.
+
+3.)Python’s interpreter is written in C language
